1549170e546af6e6ee2a25d27d21e50277e0ce63
Rich Braun <rich.braun@salesforce.com>
2020-05-25 10:14:25 -0700

    SYS-482 fix unit-test exception found on MacOS

--- apicrud/service_registry.py
+++ apicrud/service_registry.py
@@ -45,9 +45,12 @@ class ServiceRegistry(object):
     def register(self, resource_endpoints, service_name=None,
                  instance_id=socket.gethostname(), tcp_port=None):
         global service_data
+        try:
+            ipv4=socket.gethostbyname(instance_id)
+        except socket.gaierror:
+            ipv4=None
         service_data['info'] = dict(
-            endpoints=resource_endpoints,
-            ipv4=socket.gethostbyname(socket.gethostname()),
+            endpoints=resource_endpoints, ipv4=ipv4,
             port=tcp_port or self.config.PORT,
             public_url=self.config.PUBLIC_URL,
             created=utcnow().replace(microsecond=0).isoformat())
